56f94d9d8740171a90cd77399f9bab9a3211e6da
NaufalArkaan <naufalarkaan25@gmail.com>
2025-07-15 12:35:32 +0700

    test debug

---
+++ Modul 2/Belajar/src/DebugExample.java
@@ -0,0 +1,8 @@
+public class DebugExample {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int result = a + b;
+        System.out.println(result);
+    }
+}
