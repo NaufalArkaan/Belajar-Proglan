944bfd9b1133f6f2e5e569ad895f7aa2bad782f4
NaufalArkaan <naufalarkaan25@gmail.com>
2025-07-31 14:26:35 +0700

    belajar modul 5

---
+++ Modul_5/Belajar/src/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            FileWriter fileWriter = new FileWriter("data.txt");
+            fileWriter.write("Nama, NIM, Semester \n");
+            fileWriter.close();
+            System.out.println("File berhasil dibuat");
+        } catch (IOException e) {
+            System.out.println("Exception Occurred");
+        }
+    }
+}
